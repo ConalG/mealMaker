d3ca402586659af190991788b99f34d4084a28cb
ConalG <conal-glynnmartin@hotmail.com>
2021-11-30 23:39:26 +0000

    Update script.js
    work in progress

--- script.js
+++ script.js
@@ -26,8 +26,29 @@ const menu = {
         dishPrice
       };
     
-  
+      this._courses[courseName] = dish;
     }
+    getRandomDishFromCourse (courseName){
+      let dishes = this._courses[courseName];
+      let randomIndex = Math.floor(Math.random() * dishes.length);
+    },
+    
+    generateRandomMeal() {
+      const appetizer = this.getRandomDishFromCourse('appetizers');
+      const mains = this.getRandomDishFromCourse(`mains`);
+      const desserts = this.getRandomDishFromCourse(`desserts`);
+      const totalPrice = appetizer.price + mains.price + desserts.price;
+   
+      return `Your meal is ${appetizer.name}, ${main.name}, ${desserts.name}. The price is $${totalPrice}.`;
+    }
+  };
   
+    menu.addDishToCourse('appetizers', 'Caesar Salad', 4.25);
+    menu.addDishToCourse('main', 'Caesar ', 4.25);
+    menu.addDishToCourse('desserts', ' Salad', 4.25);
+    menu.addDishToCourse('main', 'Caes lad', 4.25);
+    menu.addDishToCourse('desserts', 'Caer Salad', 4.25);
+  
+   let meal = menu.generateRandomMeal();
   
   };
